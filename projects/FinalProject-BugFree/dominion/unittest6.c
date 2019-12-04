abb34b16e9869eaf72ae1f384eb55a6350e919ff
wenyu <hcwenyc@gmail.com>
2019-12-03 23:00:44 -0800

    Fix unit test 6

--- projects/FinalProject-BugFree/dominion/unittest6.c
+++ projects/FinalProject-BugFree/dominion/unittest6.c
@@ -11,16 +11,18 @@ int main (){
   int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };// declarethe game state
   struct gameState game1, game2; // declarethe arrays 
   int seed = 38;
-  int c1;
-  int c2;
-  int c3;
-  int hand;
+  int c1 = mine;
+  int c2 = 0;
+  int c3 = 0;
+  int hand = 1;
 
   printf("Unit Test 6\n");
 
   //initialize gameState
-  int r1 = initializeGame(2, k, seed, &game1);
+  initializeGame(2, k, seed, &game1);
+  game1.coins = 100;
   memcpy(&game2, &game1, sizeof(struct gameState));
+  
   //copy current gameState
 
   //call Feast
